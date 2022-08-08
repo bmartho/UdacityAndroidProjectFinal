2a1963b37ce16f02569234eb21b1ec911b7ec790
Bruno Martho <bjmartho@msn.com>
2022-08-08 06:14:21 -0300

    Implementing launcher layout

--- app/src/main/java/com/example/android/politicalpreparedness/launch/LaunchFragment.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/launch/LaunchFragment.kt
@@ -17,8 +17,8 @@ class LaunchFragment : Fragment() {
         val binding = FragmentLaunchBinding.inflate(inflater)
         binding.lifecycleOwner = this
 
-        //binding.representativeButton.setOnClickListener { navToRepresentatives() }
-        //binding.upcomingButton.setOnClickListener { navToElections() }
+        binding.representativeButton.setOnClickListener { navToRepresentatives() }
+        binding.upcomingButton.setOnClickListener { navToElections() }
 
         return binding.root
     }
